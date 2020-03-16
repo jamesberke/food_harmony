836f5988a0764cd47f8b1c3844eb69725e9d5313
Hersha Venkatesh <harshavenk2003@yahoo.com>
2020-03-16 10:38:42 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const db = require("./config/keys").mongoURI;
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require("passport");
+
+const port = process.env.PORT || 5000;
+
+const users = require("./routes/api/users");
+mongoose
+	.connect(db, { useNewUrlParser: true })
+	.then(() => console.log("Connected to MongoDB successfully!"))
+	.catch(err => console.log(err));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(passport.initialize());
+require("./config/passport")(passport);
+
+app.use("/api/users", users);
+app.listen(port, () => console.log(`Server is running on port ${port}`));
