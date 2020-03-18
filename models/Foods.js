07d59ec3259d62ae3db29e6dec3fb8f5c9c335de
Nick_Cheung <nkkcheung@gmail.com>
2020-03-18 11:15:49 -0700

    first commit

---
+++ models/Foods.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+// const Restaurant = require("./Restaurants");
+
+const FoodSchema = new Schema({
+  photoUrl: {
+    type: String
+  },
+  description: {
+    type: String
+  },
+  restaurantId: {
+    // type: Schema.Types.ObjectId
+    type: [{ type: Schema.Types.ObjectId, ref: "restaurants" }]
+    //what does this do?
+  },
+  price: {
+    type: Number
+  },
+  location: {}
+});
+
+module.exports = Food = mongoose.model("foods", FoodSchema);
