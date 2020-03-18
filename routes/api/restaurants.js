9db95220525c91b631a918dba52510c6ef52b350
Nick_Cheung <nkkcheung@gmail.com>
2020-03-18 10:21:18 -0700

    models routes

---
+++ routes/api/restaurants.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const router = express.Router();
+const jwt = require("jsonwebtoken");
+const mongoose = require("mongoose");
+const passport = require("passport");
+
+const Restaurants = require("../../models/Restaurants");
+const validateRestaurantInput = require("../../validation/restaurants");
+
+
+router.get("/test", (req, res) => res.json({ msg: "successful test" }));
+
+
+// posting new restaurant
+router.post ( "/", passport.authenticate("jwt", { session: false }),
+
+    (req, res) => {
+        const { errors, isValid } = validateRestaurantInput(req.body);
+            if (!isValid) {
+                return res.status(400).json(errors);
+            }
+
+            const newRestaurant = new Restaurant({
+                name: req.body.name,
+                food: req.body.food, //should we keep this here? or expand on it with empty strings?
+                priceRange: req.body.priceRange
+                // photoUrl: req.body.food.photoUrl,
+                // description: req.body.food.description,
+                // restaurantId: req.body.food.restaurantId,
+                // price: req.body.food.price,
+                // location: req.body.food.location,
+                // priceRange: req.body.priceRange
+                        // add these qualities in some kind of update function...
+            });
+
+            newRestaurant.save().then(restaurant => res.json(restaurant))
+    }
+);
+
+// posting photos w/ restaurant ID
+router.patch ("/:restaurantId", passport.authenticate("jwt", { session: false }),
+    (req, res) => {
+        Restaurant.findById(req.params.restaurantId, (err, restaurant) => {
+
+        })
+    }
+)
+
+module.exports = router
