6412c6ee25940f1932e9baec459e910aac728414
Nick_Cheung <nkkcheung@gmail.com>
2020-03-22 14:27:33 -0700

    heroku push

--- config/keys_dev.js
+++ config/keys_dev.js
@@ -1,3 +1,28 @@
+// 0tdC3dGrMcMx2Ydg
+// mongodb+srv://dev:0tdC3dGrMcMx2Ydg@cluster0-ehf6v.mongodb.net/test?retryWrites=true&w=majority
+
+
+// diaCMSlSTfxD6sTI
+// nkk.cheung@gmail.com
+
+
+module.exports = {
+    mongoURI:
+        "mongodb+srv://dev:diaCMSlSTfxD6sTI@cluster0-edxqb.mongodb.net/test?retryWrites=true&w=majority",
+    secretOrKey: "secret",
+    AWS_ACCESS_KEY_ID: "AKIARNH3D2GTQAOSSQTA",
+    AWS_SECRET_KEY_ID: "NRY+rmK+GiZZeVz7yU6HWL9nqf/1jlSuICkb/hZz",
+    AWS_BUCKET_NAME: "supertrampapp-ori",
+    AWS_REGION: "us-east-1",
+    AWS_Uploaded_file_URL_LINK:
+        "https://supertrampapp-ori.s3-us-west-1.amazonaws.com/"
+};
+
+
+
+
+// email cowfish813@gmail.com
+// Appacademy2020
 
 
 // old bucket policy
