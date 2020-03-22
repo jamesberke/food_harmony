ce03e07dfe6338c7a7daed253e51700c1d09f450
Nick_Cheung <nkkcheung@gmail.com>
2020-03-22 13:55:07 -0700

    keys

--- config/keys_dev.js
+++ config/keys_dev.js
@@ -1,1 +1,37 @@
 
+
+// old bucket policy
+// {
+//   "Version": "2012-10-17",
+//     "Statement": [
+//       {
+//         "Sid": "Stmt1420751757000",
+//             "Effect": "Allow",
+//         "Principal": {
+//           "AWS": "arn:aws:iam::097163399591:user/supertrampapp-master"
+//         },
+//         "Action": "s3:*",
+//         "Resource": [
+//           "arn:aws:s3:::supertrampapp-ori",
+//           "arn:aws:s3:::supertrampapp-ori/*"
+//         ]
+//       }
+//     ]
+// }
+
+// new bucket
+// {
+//   "Version": "2008-10-17",
+//     "Id": "Policy1397632521960",
+//       "Statement": [
+//         {
+//           "Sid": "Stmt1397633323327",
+//           "Effect": "Allow",
+//           "Principal": {
+//             "AWS": "*"
+//           },
+//           "Action": "s3:GetObject",
+//           "Resource": "arn:aws:s3:::supertrampapp-ori/*"
+//         }
+//       ]
+// }
