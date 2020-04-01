335ce337cddc838c911559dba332d74fe6033154
Nick_Cheung <nkkcheung@gmail.com>
2020-03-31 23:14:27 -0700

    new computer

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -5,5 +5,6 @@ module.exports = {
     AWS_SECRET_KEY_ID: process.env.AWS_SECRET_KEY_ID,
     AWS_BUCKET_NAME: process.env.AWS_BUCKET_NAME,
     AWS_REGION: process.env.AWS_REGION,
-    AWS_Uploaded_file_URL_LINK: process.env.AWS_Uploaded_file_URL_LINK
+    AWS_Uploaded_file_URL_LINK: process.env.AWS_Uploaded_file_URL_LINK,
+    GOOGLE_PLACES: process.env.GOOGLE_PLACES
 }
---
+++ models/whiteboard.js
@@ -0,0 +1,7 @@
+// throw away when done
+
+restaurants model: restauant id/other data to grab....location
+
+model to call server....something something something
+
+model needs to build it self from hitting server...
