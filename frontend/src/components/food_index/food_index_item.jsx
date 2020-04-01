d5425291b8787dd6281186d0ea7e15523cb5864b
Hersha Venkatesh <harshavenk2003@yahoo.com>
2020-04-01 16:03:39 -0700

    Finished designing foods index page

--- frontend/src/components/food_index/food_index_item.jsx
+++ frontend/src/components/food_index/food_index_item.jsx
@@ -10,6 +10,12 @@ import "./food-index.css";
 
 import { Icon, InlineIcon } from "@iconify/react";
 import heavyDollarSign from "@iconify/icons-noto/heavy-dollar-sign";
+import locationIcon from "@iconify/icons-oi/location";
+// npm install --save-dev @iconify/react @iconify/icons-fa-solid
+import locationArrow from "@iconify/icons-fa-solid/location-arrow";
+
+// npm install --save-dev @iconify/react @iconify/icons-oi
+import starIcon from "@iconify/icons-oi/star";
 
 class FoodIndexItem extends React.Component {
 	constructor(props) {
@@ -21,16 +27,34 @@ class FoodIndexItem extends React.Component {
 		let i = Math.floor(Math.random() * this.demoFoods.length);
 		return (
 			<div className="food-index-item">
-				<img src={this.demoFoods[i]} />
 				<div className="overlay">
 					<div className="overlay-content-container">
 						<div className="overlay-title">
 							{this.props.food.description}
 						</div>
-						{/* <p><div>Something</div></p>
-						<p><div>Something</div></p> */}
+
+						<div className="overlay-subtitle">
+							<i>{this.props.food.restaurantName}</i>
+						</div>
+						<div className="overlay-rating-info">
+							
+							<Icon icon={starIcon} /><Icon icon={starIcon} /><Icon icon={starIcon} /><Icon icon={starIcon} /><Icon icon={starIcon} />
+						</div>
+						<div className="overlay-details">
+							<div className="overlay-details-item-1">$ $ $</div>
+							<div className="overlay-details-item-2"></div>
+							<div className="overlay-details-item-3">
+								{/* <span>
+									<Icon icon={locationArrow} />
+								</span> */}
+								<span className="overlay-details-item-3-text">
+									10 MI
+								</span>
+							</div>
+						</div>
 					</div>
 				</div>
+				<img src={this.demoFoods[i]} />
 			</div>
 		);
 	}
