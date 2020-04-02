ea08a9ef3416ff4fb54db9f8aba405a43c210bae
Hersha Venkatesh <harshavenk2003@yahoo.com>
2020-04-02 13:46:46 -0700

    Added location upon user signup

--- frontend/src/components/session/login_form_container.js
+++ frontend/src/components/session/login_form_container.js
@@ -1,18 +1,18 @@
 import { connect } from "react-redux";
-import LoginForm from './login_form';
+import LoginForm from "./login_form";
 import { login } from "../../actions/session_actions";
 
 const mapStateToProps = state => ({
-    currentUser: state.session.user,
-    user: {
-    email: "",
-    password: ""
-    },
-    errors: state.errors.sessionErrors
+	currentUser: state.session.user,
+	user: {
+		email: "",
+		password: "",
+	},
+	errors: state.errors.sessionErrors,
 });
 
 const mapDispatchToProps = dispatch => ({
-    login: user => dispatch(login(user))
+	login: user => dispatch(login(user)),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(LoginForm);
--- frontend/src/components/session/signup_form_container.js
+++ frontend/src/components/session/signup_form_container.js
@@ -3,17 +3,20 @@ import SignupForm from "./signup_form";
 import { signup } from "../../actions/session_actions";
 
 const mapStateToProps = state => ({
-  currentUser: state.session.user,
-  user: {
-    username: "",
-    email: "",
-    password: ""
-  },
-  errors: state.errors.sessionErrors
+	currentUser: state.session.user,
+	user: {
+		firstName: "",
+		lastName: "",
+		email: "",
+		password: "",
+		password2: "",
+		location: ""
+	},
+	errors: state.errors.sessionErrors,
 });
 
 const mapDispatchToProps = dispatch => ({
-  signup: user => dispatch(signup(user))
+	signup: user => dispatch(signup(user)),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(SignupForm);
