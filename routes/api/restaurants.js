37acf80201953aece976576b164290460276656a
Nick_Cheung <nkkcheung@gmail.com>
2020-04-02 11:11:50 -0700

    keys

--- routes/api/restaurants.js
+++ routes/api/restaurants.js
@@ -6,35 +6,10 @@ const passport = require("passport");
 
 const Restaurant = require("../../models/Restaurants");
 
-const validateRestaurantInput = require("../../validation/restaurants");
+// const validateRestaurantInput = require("../../validation/restaurants");
 
 router.get("/test", (req, res) => res.json({ msg: "successful test" }));
 
-// posting new restaurant
-// router.post( "/", (req, res) => {
-//     const { errors, isValid } = validateRestaurantInput(req.body);
-//     if (!isValid) {
-//       return res.status(400).json(errors);
-//     }
-
-//     const newRestaurant = new Restaurant({
-//       name: req.body.name,
-//       food: req.body.food, //should we keep this here? or expand on it with empty strings?
-//       priceRange: req.body.priceRange
-//       // photoUrl: req.body.food.photoUrl,
-//       // description: req.body.food.description,
-//       // restaurantId: req.body.food.restaurantId,
-//       // price: req.body.food.price,
-//       // location: req.body.food.location,
-//       // priceRange: req.body.priceRange
-//       // add these qualities in some kind of update function...
-//     });
-
-//     newRestaurant.save().then(restaurant => res.json(restaurant));
-//   }
-// );
-
-// posting photos w/ restaurant ID
 
 module.exports = router;
 
@@ -98,10 +73,10 @@ router.post(
 	"/",
 	passport.authenticate("jwt", { session: false }),
 	(req, res) => {
-		const { errors, isValid } = validateRestaurantInput(req.body);
-		if (!isValid) {
-			return res.status(400).json(errors);
-		}
+		// const { errors, isValid } = validateRestaurantInput(req.body);
+		// if (!isValid) {
+		// 	return res.status(400).json(errors);
+		// }
 
 		const newRestaurant = new Restaurant({
 			name: req.body.name,
