66f2d99710bf15e8e1fa1432100a43d5a2892f39
James Berke <james.berke@gmail.com>
2020-04-08 13:52:07 -0700

    formatting to fix vulnerabilities

---
+++ config/keys_dev.js
@@ -0,0 +1,11 @@
+module.exports = {
+    mongoURI:
+        "mongodb+srv://foodking:ZwXn6J7FLjdqR7Sk@food-harmony-lgxyg.mongodb.net/test?authSource=admin&replicaSet=food-harmony-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true",
+    secretOrKey: "secret",
+    AWS_ACCESS_KEY_ID: "AKIAWGPTVOL5IUFMKMNA",
+    AWS_SECRET_KEY_ID: "Vvnf+Q2aqjKfyZ5xQrVTna7AFuJqhsQDkM97zdVq",
+    AWS_BUCKET_NAME: "supertrampapp-ori",
+    AWS_REGION: "us-east-1",
+    AWS_Uploaded_file_URL_LINK: "https://supertrampapp-ori.s3-us-west-1.amazonaws.com/",
+    GOOGLE_PLACES: "AIzaSyABsoueApieMZJykt2ttx2jizClnI5LVsg"
+};
