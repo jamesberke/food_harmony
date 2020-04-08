1c192b074d68bc3fd9d2c857df30c62f1979e8e7
Hersha Venkatesh <harshavenk2003@yahoo.com>
2020-04-07 20:31:25 -0700

    Adjusted carousel and splash page format

--- frontend/src/components/splash/splash_page.jsx
+++ frontend/src/components/splash/splash_page.jsx
@@ -1,34 +1,39 @@
 import React from "react";
-import background from './splash-background.jpg';
-import Carousel from '../carousel/carousel';
-import './splash_page.css';
+import background from "./splash-background.jpg";
+import Carousel from "../carousel/carousel";
+import "./splash_page.css";
 
 class SplashPage extends React.Component {
-  render() {
-    return (
-        <div className="splash-container">
-            <div className="splash-1">
-                <img
-                    src={background}
-                    alt="background"
-                    className="splash-background-image"
-                ></img>
-                <div className='splash-1-header'>
-                    <div className="splash-1-title">
-                        The new way to order out.
-                    </div>
-                    <div className="splash-1-text">
-                        We help provide a simple and elegant solution to quickly find your next meal.
-                    </div>
-                </div>
-                <Carousel />
-            </div>
-            <div className="splash-2">
-                
-            </div>
-        </div>
-    );
-  }
+	render() {
+		return (
+			<div className="splash-container">
+				<div className="splash-1">
+					<img
+						src={background}
+						alt="background"
+						className="splash-background-image"
+					></img>
+					<div className="splash-1-header">
+						<div className="splash-1-title">
+							The new way to order out.
+						</div>
+						<div className="splash-1-text">
+							We help provide a simple and elegant solution to
+							quickly find your next meal.
+						</div>
+
+					</div>
+					<div className="carousel-title-container">
+						<div className="splash-1-title carousel-header-text">
+							Find great options near you...
+						</div>
+					</div>
+					<Carousel />
+				</div>
+				<div className="splash-2"></div>
+			</div>
+		);
+	}
 }
 
 export default SplashPage;
