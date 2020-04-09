9eea5c0c117c282b4c101163767129e1a8af5254
James Berke <james.berke@gmail.com>
2020-04-08 17:29:26 -0700

    formatting

---
+++ config/keys_deve.js
@@ -0,0 +1,10 @@
+module.exports = {
+    mongoURI:
+    "mongodb+srv://dev:diaCMSlSTfxD6sTI@cluster0-edxqb.mongodb.net/test?retryWrites=true&w=majority",
+    secretOrKey: "secret",
+    AWS_ACCESS_KEY_ID: "AKIAWGPTVOL5CLFWXM2I",
+    AWS_SECRET_KEY_ID: "SF2kOIUFP2yQ654GsMz04N9aFcAjhaZbAuw3jnTO",
+    AWS_BUCKET_NAME: "foodharmony",
+    AWS_REGION: "us - west - 1",
+    AWS_Uploaded_file_URL_LINK: "https://foodharmony.s3-us-west-1.amazonaws.com/"
+};
