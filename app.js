5ba9611333dc81aee92957e184b1fd99af9d0bef
James Berke <james.berke@gmail.com>
2020-04-12 16:36:57 -0700

    foratting for heroku

--- app.js
+++ app.js
@@ -16,7 +16,14 @@ const restaurants = require("./routes/api/restaurants");
 
 const users = require("./routes/api/users");
 
+const path = require("path");
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('frontend/build'));
+    app.get('/', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    })
+}
 
 mongoose
 .connect(db, { useNewUrlParser: true })
